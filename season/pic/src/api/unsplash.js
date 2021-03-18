aa334e5e390c186b5719049f2d3be129ed813bbb
sbozen <31065080+sbozen@users.noreply.github.com>
2021-03-18 19:57:32 +0300

    Delete season directory

--- season/pic/src/api/unsplash.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: "https://api.unsplash.com",
-
-    headers: { Authorization: "Client-ID 6RtExMETyKyDKOE-QDYP_5EXjfEcZwmIRgY8K5zReQA", },
-});
